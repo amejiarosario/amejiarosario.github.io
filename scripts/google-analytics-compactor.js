8040b6d188c4a12e7514a2db6cd01959feacfcbd
Adrian Mejia <admejiar@cisco.com>
2016-10-30 08:22:30 -0400

    read from file

--- scripts/google-analytics-compactor.js
+++ scripts/google-analytics-compactor.js
@@ -6,38 +6,48 @@ const fs = require('fs');
 const postsPath = '../source/_posts/';
 
 function parseGaData(data){
-  let hash = {};
+  let hash = new Map();
 
   for(const row of data.rows) {
     const [pagePath, bounceRate, pageviews, avgTimeOnPage] = row;
-    const key = url.parse(pagePath);
+    const urlParsed = url.parse(pagePath);
+    const key = urlParsed.pathname.replace(/\/blog\/\d{4}\/\d{2}\/\d{2}\//,'').split(/\//)[0];
     // console.log(pagePath, pageviews);
 
-    if(hash[key.pathname]){
-      console.log('[dedup] aggregating... ', key.path);
-      let t = hash[key.pathname];
+    if(hash.get(key)) {
+      console.log('[dedup] aggregating... ', urlParsed.path);
+      let t = hash.get(key);
       t.alternatives = [] || t.alternatives;
-      t.alternatives.push(key.query);
+      t.alternatives.push(urlParsed.query);
       t.bounceRate = parseFloat(t.bounceRate, 10) + parseFloat(bounceRate, 10);
       t.pageviews = parseFloat(t.pageviews, 10) + parseFloat(pageviews, 10);
       t.avgTimeOnPage = parseFloat(t.avgTimeOnPage, 10) + parseFloat(avgTimeOnPage, 10);
+
+      hash.set(key, t);
     } else {
-      hash[key.pathname] = {
+      hash.set(key, {
         bounceRate,
         pageviews,
         avgTimeOnPage
-      }
+      });
     }
   }
+
   // console.log(hash);
+  return hash;
 }
 
-function updateBlog(hash){
+function updateBlog(hash) {
   fs.readdir(postsPath, (err, files) => {
     console.log(err);
-    files.forEach(file => {
-      console.log(file);
-    });
+    for(const file of files) {
+      const [filename, ext] = file.split('.');
+      const key = filename.replace(/\d{4}-\d{2}-\d{2}-/, '');
+      console.log(hash.get(key));
+      if(hash.get(key)){
+
+      }
+    }
   });
 }
 
