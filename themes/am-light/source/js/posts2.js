12e9d8886064a57d6d73ea621248ce2a214a1a5f
Adrian Mejia <adriansky@gmail.com>
2020-08-12 10:50:08 -0400

    tree and array working side-by-side

--- themes/am-light/source/js/posts2.js
+++ themes/am-light/source/js/posts2.js
@@ -2,6 +2,7 @@ console.log('hello')
 
 class ScrollSpy {
   tocItemsByLevels = [];
+  tocTree = [];
   docItems = [];
   currentActives = [];
 
@@ -16,6 +17,10 @@ class ScrollSpy {
     for (let level = 1; level <= this.maxLevel; level++) {
       this.tocItemsByLevels[level] = Array.from(document.querySelectorAll(`.toc-level-${level} > a`)).reverse();
     }
+
+    // revese needed for easy find last on #setActiveClasses
+    const traverseTree = el => Array.from((el?.children[1]?.children || [])).map(child => ({item: child, children: traverseTree(child) })).reverse(); // .toc-child > .toc-item
+    this.tocTree = Array.from(document.querySelectorAll('.toc > .toc-item')).map(item => ({item, children: traverseTree(item) })).reverse();
   }
 
   calculateDocItemsOffsets() {
@@ -31,25 +36,51 @@ class ScrollSpy {
         el.setAttribute('data-doc-offset', offset);
       });
     }
+
+    const traverseTree = (root) => {
+      root.forEach(({item, children}) => {
+        const id = item.querySelector('.toc-link').href.split('#')[1];
+        const el = document.getElementById(decodeURIComponent(id)); // in case of emoji was used
+        const offset = el.offsetTop - 1;
+        item.setAttribute('data-offset', offset);
+
+        traverseTree(children);
+      });
+    }
+    traverseTree(this.tocTree);
   }
 
   setActiveClasses() {
     this.removeCurrentClasses();
     const current = getScrollY();
 
-    for (let level = 1; level <= this.maxLevel; level++) {
-      const doc = this.tocItemsByLevels[level].find((el) => {
-        const offset = Number(el.getAttribute('data-doc-offset'));
-        console.log({level, el, offset, current});
-        return current >= offset;
-      });
-      if (doc) {
-        const parent = doc.closest('.toc-level-2');
-        doc.classList.add(this.activeClass);
-        parent.classList.add(this.activeClass);
-        this.currentActives.push(doc, parent);
+    // for (let level = 1; level <= this.maxLevel; level++) {
+    //   const doc = this.tocItemsByLevels[level].find((el) => {
+    //     const offset = Number(el.getAttribute('data-doc-offset'));
+    //     // console.log({level, el, offset, current});
+    //     return current >= offset;
+    //   });
+    //   if (doc) {
+    //     const parent = doc.closest('.toc-level-2');
+    //     doc.classList.add(this.activeClass);
+    //     parent.classList.add(this.activeClass);
+    //     this.currentActives.push(doc, parent);
+    //   }
+    // }
+
+    const traverseTree = (root) => {
+      const index = root.findIndex(d => current >= Number(d.item.getAttribute('data-offset')));
+      console.log({index, root})
+      if (index >= 0) {
+        const {item, children} = root[index];
+        const link = item.querySelector('.toc-link');
+        item.classList.add(this.activeClass); // li
+        link.classList.add(this.activeClass); // a
+        this.currentActives.push(item, link);
+        traverseTree(children);
       }
     }
+    traverseTree(this.tocTree);
   }
 
   removeCurrentClasses() {
