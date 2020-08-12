660f10e9b23e0245da8542627a211630b5ec28e1
Adrian Mejia <adriansky@gmail.com>
2020-08-12 10:59:29 -0400

    cleanup

--- themes/am-light/source/js/posts2.js
+++ themes/am-light/source/js/posts2.js
@@ -1,101 +1,68 @@
-console.log('hello')
+// wait for everything to be loaded
+document.addEventListener("DOMContentLoaded", () => {
+  try {
+    class ScrollSpy {
+      tocTree = [];
+      currentActives = [];
 
-class ScrollSpy {
-  tocItemsByLevels = [];
-  tocTree = [];
-  docItems = [];
-  currentActives = [];
+      constructor(activeClass = 'active', tocLevelPrefixClass = '.toc-level-', maxLevel = 6) {
+        this.tocLevelPrefixClass = tocLevelPrefixClass;
+        this.activeClass = activeClass;
+        this.maxLevel = maxLevel;
+      }
 
-  constructor(activeClass = 'active', tocLevelPrefixClass = '.toc-level-', maxLevel = 6) {
-    this.tocLevelPrefixClass = tocLevelPrefixClass;
-    this.activeClass = activeClass;
-    this.maxLevel = maxLevel;
-  }
+      getTocItems() {
+        // revese needed for easy find last on #setActiveClasses
+        const traverseTree = el => Array.from((el?.children[1]?.children || [])).map(child => ({item: child, children: traverseTree(child) })).reverse(); // .toc-child > .toc-item
+        this.tocTree = Array.from(document.querySelectorAll('.toc > .toc-item')).map(item => ({item, children: traverseTree(item) })).reverse();
+      }
 
-  getTocItems() {
-    this.tocItemsByLevels = [];
-    for (let level = 1; level <= this.maxLevel; level++) {
-      this.tocItemsByLevels[level] = Array.from(document.querySelectorAll(`.toc-level-${level} > a`)).reverse();
-    }
+      calculateDocItemsOffsets() {
+        const traverseTree = (root) => {
+          root.forEach(({item, children}) => {
+            const id = item.querySelector('.toc-link').href.split('#')[1];
+            const el = document.getElementById(decodeURIComponent(id)); // in case of emoji was used
+            const offset = el.offsetTop - 1;
+            item.setAttribute('data-offset', offset);
 
-    // revese needed for easy find last on #setActiveClasses
-    const traverseTree = el => Array.from((el?.children[1]?.children || [])).map(child => ({item: child, children: traverseTree(child) })).reverse(); // .toc-child > .toc-item
-    this.tocTree = Array.from(document.querySelectorAll('.toc > .toc-item')).map(item => ({item, children: traverseTree(item) })).reverse();
-  }
+            traverseTree(children);
+          });
+        }
+        traverseTree(this.tocTree);
+      }
 
-  calculateDocItemsOffsets() {
-    for (let level = 1; level <= this.maxLevel; level++) {
-      this.tocItemsByLevels[level].forEach((el) => {
-        // console.log({level, el});
-        const id = el.href.split('#')[1];
-        const doc = document.getElementById(decodeURIComponent(id)); // in case of emoji was used
-        // console.log({id, doc})
-        const offset = doc.offsetTop - 1;
-        // modify in-place
-        // el.docOffsetTop = offset;
-        el.setAttribute('data-doc-offset', offset);
-      });
-    }
+      setActiveClasses() {
+        this.removeCurrentClasses();
+        const current = getScrollY();
 
-    const traverseTree = (root) => {
-      root.forEach(({item, children}) => {
-        const id = item.querySelector('.toc-link').href.split('#')[1];
-        const el = document.getElementById(decodeURIComponent(id)); // in case of emoji was used
-        const offset = el.offsetTop - 1;
-        item.setAttribute('data-offset', offset);
+        const traverseTree = (root) => {
+          const index = root.findIndex(d => current >= Number(d.item.getAttribute('data-offset')));
+          // console.log({index, root})
+          if (index >= 0) {
+            const {item, children} = root[index];
+            const link = item.querySelector('.toc-link');
+            item.classList.add(this.activeClass); // li
+            link.classList.add(this.activeClass); // a
+            this.currentActives.push(item, link);
+            traverseTree(children);
+          }
+        }
+        traverseTree(this.tocTree);
+      }
 
-        traverseTree(children);
-      });
-    }
-    traverseTree(this.tocTree);
-  }
-
-  setActiveClasses() {
-    this.removeCurrentClasses();
-    const current = getScrollY();
-
-    // for (let level = 1; level <= this.maxLevel; level++) {
-    //   const doc = this.tocItemsByLevels[level].find((el) => {
-    //     const offset = Number(el.getAttribute('data-doc-offset'));
-    //     // console.log({level, el, offset, current});
-    //     return current >= offset;
-    //   });
-    //   if (doc) {
-    //     const parent = doc.closest('.toc-level-2');
-    //     doc.classList.add(this.activeClass);
-    //     parent.classList.add(this.activeClass);
-    //     this.currentActives.push(doc, parent);
-    //   }
-    // }
-
-    const traverseTree = (root) => {
-      const index = root.findIndex(d => current >= Number(d.item.getAttribute('data-offset')));
-      console.log({index, root})
-      if (index >= 0) {
-        const {item, children} = root[index];
-        const link = item.querySelector('.toc-link');
-        item.classList.add(this.activeClass); // li
-        link.classList.add(this.activeClass); // a
-        this.currentActives.push(item, link);
-        traverseTree(children);
+      removeCurrentClasses() {
+        this.currentActives.forEach(el => el.classList.remove(this.activeClass));
+        this.currentActives = [];
       }
     }
-    traverseTree(this.tocTree);
+
+    const scrollSpy = new ScrollSpy();
+    scrollSpy.getTocItems();
+    scrollSpy.calculateDocItemsOffsets();
+
+    window.addEventListener("scroll", () => scrollSpy.setActiveClasses());
+    window.addEventListener("resize", () => scrollSpy.calculateDocItemsOffsets());
+  } catch (error) {
+    console.info(error)
   }
-
-  removeCurrentClasses() {
-    this.currentActives.forEach(el => el.classList.remove(this.activeClass));
-    this.currentActives = [];
-  }
-}
-
-const scrollSpy = new ScrollSpy();
-
-// wait for everything to be loaded
-document.addEventListener("DOMContentLoaded", (event) => {
-  scrollSpy.getTocItems();
-  scrollSpy.calculateDocItemsOffsets();
-
-  window.addEventListener("scroll", () => scrollSpy.setActiveClasses());
-  window.addEventListener("resize", () => scrollSpy.calculateDocItemsOffsets());
 });
